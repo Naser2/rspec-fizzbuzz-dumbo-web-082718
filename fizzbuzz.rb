e6908dafc5eaf745ab32f030fff6e06375fe415b
Nasser Sanou <nassersanou23@gmail.com>
2018-08-19 20:09:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,7 +12,7 @@ def fizzbuzz(num)
     elsif num % 5 == 0  && num % 3 !=0
     return "Buzz"
 
-    elsif  !(num % 3 == 0) && !(num % 5 == 0)
+    elsif  (num % 3 == 0) && (num % 5 == 0)
     return nil
     else 
     return "Fizzbuzz"
