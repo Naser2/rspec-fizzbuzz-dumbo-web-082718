3c774ca4ff16652ae3477f365f7c68801f410636
Nasser Sanou <nassersanou23@gmail.com>
2018-08-19 20:07:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,16 +4,21 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(num, num1)
-   if num % 3 == 0 
+def fizzbuzz(num)
+
+   if num % 3 == 0  &&  num % 5 !=0
     return "Fizz"
-    elsif num % 5 == 0 
+
+    elsif num % 5 == 0  && num % 3 !=0
     return "Buzz"
-    elsif (num % 3 == 0 && num1 % 5 == 0)
-    return "fizzbuzz"
+
+    elsif  !(num % 3 == 0) && !(num % 5 == 0)
+    return nil
+    else 
+    return "Fizzbuzz"
    end 
   end 
-  fizzbuzz(3 && 5)
+  fizzbuzz(9)
   
 # if num % 3 == 0 && num % 5 == 0 
 #   return "Fizzbuzz"
